6c6b55b64d8fbc97bc5bbbdfbd6fefc8977efc67
martin-ngigi <martinwainaina001@gmail.com>
2022-10-21 08:48:39 +0300

    Send email successful

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Send Email</title>
+</head>
+<body>
+	<form class="" action="send.php" method="post">
+		Emai <input type="emai" name="email" value=""> <br>
+		Subject <input type="text" name="subject" value=""> <br>
+		Message <input type="text" name="message" value=""> <br>
+		<button type="submit" name="send">Send</button>
+		
+	</form>
+</body>
+</html>
---
+++ send.php
@@ -0,0 +1,53 @@
+<?php 
+// Import PHPMailer classes into the global namespace 
+use PHPMailer\PHPMailer\PHPMailer; 
+use PHPMailer\PHPMailer\SMTP; 
+use PHPMailer\PHPMailer\Exception; 
+ 
+// Include library files 
+require 'PHPMailer/src/Exception.php'; 
+require 'PHPMailer/src/PHPMailer.php'; 
+require 'PHPMailer/src/SMTP.php'; 
+
+if (isset($_POST["send"])) {
+	# code...
+}
+ 
+// Create an instance; Pass `true` to enable exceptions 
+$mail = new PHPMailer(true); 
+ 
+// Server settings 
+//$mail->SMTPDebug = SMTP::DEBUG_SERVER;    //Enable verbose debug output 
+$mail->isSMTP();                            // Set mailer to use SMTP 
+$mail->Host = 'smtp.gmail.com';           // Specify main and backup SMTP servers 
+$mail->SMTPAuth = true;                     // Enable SMTP authentication 
+$mail->Username = 'martinwainaina001@gmail.com';       // SMTP username 
+$mail->Password = 'qeyviwbzgrjphghz';         // SMTP password 
+$mail->SMTPSecure = 'ssl';                  // Enable TLS encryption, `ssl` also accepted 
+$mail->Port = 465;                          // TCP port to connect to 
+ 
+// Sender info 
+$mail->setFrom('martinwainaina001@gmail.com', 'Martin Wainaina'); 
+$mail->addReplyTo('martinwainaina001@gmail.com', 'Martin Wainaina'); 
+ 
+// Add a recipient 
+$mail->addAddress($_POST["email"]); 
+ 
+//$mail->addCC('cc@example.com'); 
+//$mail->addBCC('bcc@example.com'); 
+ 
+// Set email format to HTML 
+$mail->isHTML(true); 
+ 
+// Mail subject 
+$mail->Subject = $_POST["subject"]; 
+ 
+// Mail body content  
+$mail->Body    = $_POST["message"]; 
+ 
+// Send email 
+if(!$mail->send()) { 
+    echo 'Message could not be sent. Mailer Error: '.$mail->ErrorInfo; 
+} else { 
+    echo 'Message has been sent.'; 
+}
